1bf62c6486890bb8f455274cc2b42b2790a1044d
pauliejes <pauliejes@gmail.com>
2020-02-18 23:42:41 -0500

    4THEclient

--- CosmosDBResourceTokenBroker.Shared/CosmosDBRepository.cs
+++ CosmosDBResourceTokenBroker.Shared/CosmosDBRepository.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Microsoft.Azure.Documents;
-using Microsoft.Azure.Documents.Client;
+// using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.Documents.Linq;
 
 using CosmosDBResourceTokenBroker.Shared.Interface;
